7d1d2ac99fa4a544a8682aa40a199644478868e3
Florian R. Hanke <florian.hanke@gmail.com>
2015-05-03 11:37:49 +0200

    Remove try compile.

--- lib/numbray.rb
+++ lib/numbray.rb
@@ -1,1 +1,18 @@
-require_relative 'try_compile'
+begin
+  require ::File.expand_path '../numbray/numbray', __FILE__
+rescue LoadError => e
+  # Give up and inform the user.
+  #
+  puts <<-NOTE
+
+Numbray tried to compile its source on your system but failed.
+
+Please add an issue: https://github.com/floere/Numbray/issues/
+and copy anything into it that you think is helpful. Thanks!
+
+The problem reported by the compiler was:
+#{e}
+
+NOTE
+  exit 1
+end
