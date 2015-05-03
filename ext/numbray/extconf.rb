69c9dacce1d52e49720e5a8e7ca3811e67c53c10
Florian R. Hanke <florian.hanke@gmail.com>
2015-05-03 11:36:08 +0200

    Add extconf.

---
+++ ext/numbray/extconf.rb
@@ -0,0 +1,16 @@
+# Information.
+#
+puts
+print "Compiling on Ruby #{RUBY_VERSION}"
+if defined?(RbConfig)
+  RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC'] 
+  print " with CC set to #{RbConfig::MAKEFILE_CONFIG['CC']}"
+end
+puts "."
+
+# Compile.
+#
+require 'mkmf'
+abort 'need ruby.h' unless have_header("ruby.h")
+create_makefile 'numbray/numbray'
+puts
